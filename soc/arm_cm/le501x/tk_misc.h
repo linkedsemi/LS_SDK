9f646f2ca83008f016694673246ecc0a82b37086
Xintian Zhang <xtzhang@linkedsemi.com>
2022-03-07 11:06:28 +0000

    5030 tk

---
+++ soc/arm_cm/le501x/tk_misc.h
@@ -0,0 +1,23 @@
+#ifndef __TK_MISC_H
+#define __TK_MISC_H
+
+#define IS_TK_SCANCFG_NPRR(NPRR) (((NPRR) >= 0U) && ((NPRR) <= 7))
+#define IS_TK_SCANCFG_DNPR(DNPR) (((DNPR) == TK_DNPR_DISABLE) || \
+                                  ((DNPR) == TK_DNPR_ENABLE))
+#define IS_TK_SCANCFG_MULTION(MULTION) (((MULTION) == TK_MULTION_DISABLE) || \
+                                        ((MULTION) == TK_MULTION_ENABLE))
+#define IS_TK_DEBOUNCE(DEBOUNCE) (((DEBOUNCE) >= 0U) && ((DEBOUNCE) <= 255U))
+#define IS_TK_CHANNEL_EN(CHANNELEN) (((CHANNELEN)&0x0000U) == 0U)
+#define IS_TK_GBSEN(GBSEN) (((GBSEN) >= 0U) && ((GBSEN) <= 255U))
+#define IS_TK_SCANCR_LP(LP) (((LP) == TK_LP_DISABLE) || \
+                             ((LP) == TK_LP_ENABLE))
+#define IS_TK_ICOMGS(ICOMGS) (((ICOMGS)&0x00U) == 0U)
+#define IS_TK_CDCFSWDIV(CDCFSWDIV) (((CDCFSWDIV)&0x00U) == 0U)
+#define IS_TK_LOW_POWER_SCAN_PERIOD(LOWPOWERSCANPERIOD) (((LOWPOWERSCANPERIOD) >= 0U) && ((LOWPOWERSCANPERIOD) <= 2U))
+#define IS_TK_KEY_DEBOUNCE_COUNT(KEYDEBOUNCECOUNT) (((KEYDEBOUNCECOUNT) > 0U) && ((KEYDEBOUNCECOUNT) <= 3U))
+#define IS_TK_LP_DELAY(LPDELAY) (((LPDELAY) >= 0U) && ((LPDELAY) <= 255U))
+#define IS_TK_SCAN_PERIOD(SCANPERIOD) (((SCANPERIOD) > 0U) && ((SCANPERIOD) <= 3U))
+#define IS_TK_GSMASK(GSMASK) (((GSMASK)&0x0000U) == 0U)
+#define IS_TK_LSENSEGS(LSENSEGS) (((LSENSEGS)&0x00U) == 0U)
+
+#endif
