484c0d4d47875738d9b2d23817042bd9f2a3ce95
mingzhou-ls <mingzhou@linkedsemi.com>
2022-06-18 17:14:43 +0800

    Add general_fifo_get/set api in fifo.

--- module/static_buffer/fifo.c
+++ module/static_buffer/fifo.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
 #include "fifo.h"
 #include "compile_flag.h"
 #define GET_WR_IDX(fifo) ((fifo)->wr_idx>=(fifo)->length?(fifo)->wr_idx - (fifo)->length:(fifo)->wr_idx)
 #define GET_RD_IDX(fifo) ((fifo)->rd_idx>=(fifo)->length?(fifo)->rd_idx - (fifo)->length:(fifo)->rd_idx)
 
-ROM_SYMBOL void sw_fifo_init(struct fifo_env *ptr,void *buf,uint16_t length)
+ROM_SYMBOL void sw_fifo_init(struct fifo_env *ptr,void *buf,uint16_t length, uint16_t item_size)
 {
     ptr->buf = buf;
     ptr->rd_idx = 0;
     ptr->wr_idx = 0;
     ptr->length = length;
+    ptr->item_size = item_size;
 }
 
 ROM_SYMBOL LL_PKT_ISR bool sw_fifo_full(struct fifo_env *ptr)
@@ -32,6 +34,26 @@ ROM_SYMBOL uint16_t sw_fifo_element_amount(struct fifo_env *ptr)
     return ptr->wr_idx-ptr->rd_idx;
 }
 
+ROM_SYMBOL LL_PKT_ISR bool general_fifo_put(struct fifo_env *ptr,void *data)
+{
+    if(sw_fifo_full(ptr))
+    {
+        return false;
+    }else
+    {
+        uint8_t *elem = ptr->buf;
+        memcpy((void*)(elem + ptr->item_size * GET_WR_IDX(ptr)), data, ptr->item_size);
+        if(ptr->wr_idx + 1 == 2*ptr->length)
+        {
+            ptr->wr_idx = 0;
+        }else
+        {
+            ptr->wr_idx = ptr->wr_idx + 1;
+        }
+        return true;
+    }
+}
+
 ROM_SYMBOL LL_PKT_ISR bool dword_fifo_put(struct fifo_env *ptr,void *data)
 {
     if(sw_fifo_full(ptr))
@@ -47,6 +69,26 @@ ROM_SYMBOL LL_PKT_ISR bool dword_fifo_put(struct fifo_env *ptr,void *data)
         }else
         {
             ptr->wr_idx = ptr->wr_idx + 1;
+        }
+        return true;
+    }
+}
+
+ROM_SYMBOL bool general_fifo_get(struct fifo_env *ptr,void *data)
+{
+    if(sw_fifo_empty(ptr))
+    {
+        return false;
+    }else
+    {
+        uint8_t *elem = ptr->buf;
+        memcpy(data,(void*)(elem + ptr->item_size * GET_RD_IDX(ptr)), ptr->item_size);
+        if(ptr->rd_idx + 1 == 2*ptr->length)
+        {
+            ptr->rd_idx = 0;
+        }else
+        {
+            ptr->rd_idx = ptr->rd_idx + 1;
         }
         return true;
     }
