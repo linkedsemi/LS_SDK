f8869553ec9cba45c365a050da67b217999c7a4c
luojunyang <jyluo@linkedsemi.com>
2023-07-14 03:27:14 +0000

    Iap link change

--- examples/peripheral/iap/iap_bootloader/uart_process.c
+++ examples/peripheral/iap/iap_bootloader/uart_process.c
@@ -5,19 +5,17 @@
 #include "ls_hal_uart.h"
 #include "systick.h"
 #include "uart_process.h"
+#include "log.h"
+
+uart_receive_data_t uart_buffer;
+uart_receive_data_t uart_cmd;
 
 UART_HandleTypeDef UART_Config;
-
-uint8_t uart_buffer[BUF_SIZE];
-uint16_t uart_buffer_len = 0;
-
 uint8_t uart_recv_char;
 
-void uart_var_init(void)
-{
-    memset(uart_buffer, 0, BUF_SIZE);
-    uart_buffer_len = 0;
-}
+// uint32_t uart_recv_ticker;
+// uint8_t uart_rx_flag_step;
+
 void uart_init(uint8_t txd, uint8_t rxd)
 {
     // pinmux_uart1_init(PA09,PA10);
@@ -35,38 +33,71 @@ void uart_init(uint8_t txd, uint8_t rxd)
     HAL_UART_Init(&UART_Config);
 }
 
-void uart_rx(void)
-{
-    HAL_UART_Receive_IT(&UART_Config, &uart_recv_char, 1);
-}
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-    if (uart_buffer_len >= BUF_SIZE)
+    uint16_t len;
+    uint8_t rx_char;
+
+    if (huart->UARTX == UART1)
     {
-        uart_var_init();
+        rx_char = uart_recv_char;
+        HAL_UART_Receive_IT(&UART_Config, &uart_recv_char, 1);
+
+        if (uart_buffer.uRxFlag == RX_FLAG_HEAD)
+        {
+            uart_buffer.uRxSize = 0;
+            uart_buffer.uRxFlag = RX_FLAG_RECEIVING;
+        }
+        else if (uart_buffer.uRxSize >= BUF_SIZE)
+        {
+            uart_buffer.uRxSize = 0;
+            uart_buffer.uRxFlag = RX_FLAG_RECEIVING;
+        }
+        uart_buffer.aRxBuff[uart_buffer.uRxSize++] = rx_char;
+
+        if (uart_buffer.uRxSize >= 8)
+        {
+            if (uart_buffer.aRxBuff[0] == SOD)
+            {
+                uart_buffer.uDataLen = (((uint16_t)uart_buffer.aRxBuff[2]) << 8) + ((uint16_t)uart_buffer.aRxBuff[3]);
+                len = uart_buffer.uDataLen + 6 - 1;
+                //len = uart_buffer.uRxSize - 1; // last char
+
+                if (uart_buffer.aRxBuff[len] == EOD)
+                {
+                    uart_buffer.uRxFlag = RX_FLAG_COMPLETION;
+                    memcpy(&uart_cmd, &uart_buffer, sizeof(uart_receive_data_t));
+                    memset(&uart_buffer, 0x00, sizeof(uart_receive_data_t));
+                    // uart_buffer.uRxSize=0;
+                    uart_buffer.uRxFlag = RX_FLAG_RECEIVING;
+                }
+            }
+        }
     }
-    uart_buffer[uart_buffer_len++] = uart_recv_char;
-    uart_rx();
 }
 
 void uart_receive_init(void)
 {
-    uart_var_init();
-    uart_rx();
+    memset(&uart_buffer, 0, sizeof(uart_buffer));
+    uart_buffer.uRxFlag = RX_FLAG_HEAD;
+
+    HAL_UART_Receive_IT(&UART_Config, &uart_recv_char, 1);
 }
-void uart_send_receive_data(void)
-{
-    uart_send(uart_buffer, uart_buffer_len);
-}
+
 void uart_send(uint8_t *data, uint16_t data_len)
 {
     if ((NULL == data) || (0 == data_len))
     {
         return;
     }
-    HAL_UART_Transmit(&UART_Config, data, data_len, 1000);
+    #if defined(LE501X)
+    HAL_UART_Transmit_IT(&UART_Config, data, data_len);
+    #elif defined(LM3050)
+    HAL_UART_Transmit(&UART_Config, data, data_len,1000);
+    #endif
+    
 }
+
 void uart_send_string(uint8_t *str)
 {
     uint8_t *data = str;
@@ -76,9 +107,11 @@ void uart_send_string(uint8_t *str)
         str++;
         data_len++;
     }
-    HAL_UART_Transmit(&UART_Config, data, data_len, 1000);
+    // HAL_UART_Transmit(&UART_Config, data, data_len, 1000);
+    HAL_UART_Transmit_IT(&UART_Config, data, data_len);
+    DELAY_US(50000);
 }
-
+/*
 int32_t ticker_out(uint32_t *ticker, uint32_t compare_value)
 {
     uint32_t ticks = 0;
@@ -109,3 +142,4 @@ int32_t ticker_out(uint32_t *ticker, uint32_t compare_value)
 
     return 0;
 }
+*/
