38747e4942121025387f1fec57d6013c05bbee4f
Hunter Liu <htliu@linkedsemi.com>
2024-10-18 14:45:31 +0800

    field_manipulate.h: optimize function logic

--- soc/field_manipulate.h
+++ soc/field_manipulate.h
@@ -6,36 +6,36 @@
 extern "C" {
 #endif
 
-#define REG_FIELD_WR(reg,field,val)\
-        do{                                 \
-        uint32_t old_val = (reg);\
-        uint32_t new_val = ((unsigned int)(val)<<(field##_POS)&(unsigned int)(field##_MASK))|(old_val &~(unsigned int)(field##_MASK));\
-        (reg)= new_val;\
-    }while(0)
-
-#define REG_FIELD_RD(reg,field)\
-    (((reg)&(unsigned int)(field##_MASK))>>(field##_POS))
-
-#define FIELD_BUILD(field,val) \
-    ((unsigned int)(val)<<(field##_POS)&(unsigned int)(field##_MASK))
-
 /** @addtogroup Exported_macros
   * @{
   */
-#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
-
-#define CLEAR_BIT(REG, BIT)   ((REG) &= ~(BIT))
-
-#define READ_BIT(REG, BIT)    ((REG) & (BIT))
-
-#define CLEAR_REG(REG)        ((REG) = (0x0))
-
 #define WRITE_REG(REG, VAL)   ((REG) = (VAL))
 
-#define READ_REG(REG)         ((REG))
+#define READ_REG(REG)         (REG)
+
+#define SET_BIT(REG, BIT)     (WRITE_REG(REG, (READ_REG(REG) | (BIT))))
+
+#define CLEAR_BIT(REG, BIT)   (WRITE_REG(REG, (READ_REG(REG) & ~(BIT))))
+
+#define READ_BIT(REG, BIT)    ((READ_REG(REG)) & (BIT))
+
+#define CLEAR_REG(REG)        (WRITE_REG(REG, 0x0))
 
 #define MODIFY_REG(REG, CLEARMASK, SETMASK)  \
         WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+
+#define REG_FIELD_WR(reg,field,val)\
+        do{                                 \
+        uint32_t old_val = READ_REG(reg);\
+        uint32_t new_val = ((unsigned int)(val)<<(field##_POS)&(unsigned int)(field##_MASK))|(old_val &~(unsigned int)(field##_MASK));\
+        WRITE_REG(reg, new_val);\
+    }while(0)
+
+#define REG_FIELD_RD(reg,field)\
+    (((READ_REG(reg))&(unsigned int)(field##_MASK))>>(field##_POS))
+
+#define FIELD_BUILD(field,val) \
+    ((unsigned int)(val)<<(field##_POS)&(unsigned int)(field##_MASK))
 
 #ifdef __cplusplus
 }
