99710a9fcbf9144ea80aba04a3b1e92e2ae70850
Hunter Liu <htliu@linkedsemi.com>
2025-01-03 16:48:59 +0800

    qsh: split per_func_mux.h

---
+++ soc/rv32/qsh/per_func_mux_type.h
@@ -0,0 +1,20 @@
+#ifndef PER_FUNC_MUX_TYPE_H_
+#define PER_FUNC_MUX_TYPE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PINMUX_FUNC1 0
+#define PINMUX_FUNC2 1
+#define PINMUX_FUNC3 2
+#define PINMUX_FUNC4 3
+
+#define PINMUX_FUNC_START PINMUX_FUNC1
+#define PINMUX_FUNC_END   PINMUX_FUNC4
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
