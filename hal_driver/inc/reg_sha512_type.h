8e4f6c457a2160c19ff8b5016602da88988a243f
hqyang77 <hqyang77@gmail.com>
2025-04-08 16:45:48 +0800

    update  sha512 reg

--- hal_driver/inc/reg_sha512_type.h
+++ hal_driver/inc/reg_sha512_type.h
@@ -12,12 +12,47 @@ typedef struct
     volatile uint32_t INTR_RAW;
     volatile uint32_t INTR_STT;
     volatile uint32_t CTRL; 
-    volatile uint32_t RACTOR_NUM;
+    volatile uint32_t FACTOR_NUM;
     volatile uint32_t ADDR;
     volatile uint32_t STATUS;
     volatile uint32_t RESERVED[40];//0x20
     volatile uint32_t DIGEST[16];
 } reg_sha512_t;
+
+enum SHA512_REG_INTR_FIELD
+{
+    SHA512_INTR_DMA_END_MASK = (int)0x1,
+    SHA512_INTR_DMA_END_POS = 0,
+    SHA512_INTR_CALC_END_MASK = (int)0x2,
+    SHA512_INTR_CALC_END_POS = 1,
+};
+
+enum SHA512_REG_CTRL_FIELD
+{
+    SHA512_CTRL_INIT_CALC_MASK = (int)0x1,
+    SHA512_CTRL_INIT_CALC_POS = 0,
+    SHA512_CTRL_START_MASK = (int)0x2,
+    SHA512_CTRL_START_POS = 1,
+    SHA512_CTRL_MODE_MASK = (int)0xc,
+    SHA512_CTRL_MODE_POS = 2,
+    SHA512_CTRL_FACTOR_MASK = (int)0x80,
+    SHA512_CTRL_FACTOR_POS = 7,
+    SHA512_CTRL_BLOCK_NUM_MASK = (int)0xffff0000,
+    SHA512_CTRL_BLOCK_NUM_POS = 16,
+};
+
+enum SHA512_REG_FACTOR_NUM_FIELD
+{
+    SHA512_FACTOR_NUM_MASK = (int)0xffffffff,
+    SHA512_FACTOR_NUM_POS = 0,
+};
+
+enum SHA512_REG_ADDR_FIELD
+{
+    SHA512_ADDR_MASK = (int)0xffffffff,
+    SHA512_ADDR_POS = 0,
+};
+
 
 
 #ifdef __cplusplus
