1c7cb28f176c077929dc98ae1a4799253a736fb9
hu zhiwu <zwhu@linkedsemi.com>
2025-06-11 01:22:14 +0000

    使用一个变量，对lsi的校准周期进行控制

--- examples/ble/ble_dis/main.c
+++ examples/ble/ble_dis/main.c
@@ -61,9 +61,15 @@ static void gap_manager_callback(enum gap_evt_type type,union gap_evt_u *evt,uin
     switch(type)
     {
     case CONNECTED:
-
+        /* 连上之后，修改lsi校准周期，从原来的60s校准一次，修改为10s校准一次,且连上之后，立即执行一次校准 */
+        /* 这个校准，主要是保证在lp0时，电源供电不那么稳定，温度变化明显，或者其他因素，对lsi有较大影响的时候对lsi进行及时校准，使得lsi更加准确 */
+        /* 判断lsi是否受到较大影响，可以通过进入lp0,主从即使靠得很近，依然容易出现断连。但是有外设在工作，不进入lp0,这个时候，不容易出现断连来判断。 */
+        /* 这个修改对于连接功耗会有一定的影响，每次校准，持续的时间为内部lsi计数1500个周期的时间，大概为1000 / 32768 * 1500 = 45ms左右的时间 */
+        platform_set_lsi_recount_period_ms(10 * 1000, true);
     break;
     case DISCONNECTED:
+        /* 广播的时候，对于lsi的精度要求不是那么高，可以使用60s的默认校准周期，最大可以设置为30分钟校准一次，这可以降低一些广播功耗 */
+        platform_set_lsi_recount_period_ms(LSI_DEFAULT_RECOUNT_PERIOD_MS, false);
         dev_manager_start_adv(adv_obj_hdl,advertising_data,sizeof(advertising_data),scan_response_data,sizeof(scan_response_data));
     break;
     case CONN_PARAM_REQ:
