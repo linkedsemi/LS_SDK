b4ab71a3cf8c742733402040fd8b40c4cd5ceceb
ry hou <ryhou@linkedsemi.com>
2025-08-01 08:00:50 +0000

    modify_fw_desc_gen

--- tools/leo/fw_desc_gen.py
+++ tools/leo/fw_desc_gen.py
@@ -2,6 +2,7 @@ import sys
 import struct
 import zlib
 import os
+from intelhex import IntelHex
 
 COPY_TO_RAM = 0
 FLASH_CHIP_ERASE = 1
@@ -9,6 +10,48 @@ RAM_DST_ADDR = 0x0
 RAM_ENTRY_ADDR = 0x0
 PADDING_ENABLE = 0x1
 
+APP_DESC_OFFSET = 0x2000 # app在bin文件中的偏移
+APP_OFFSET = 0x2060
+APP_SIGN_OFFSET = APP_OFFSET - 0x40
+INFO_SBL_DATA_LEN = 0x2000
+# 生成最终的BIN文件
+fw_bin = open(sys.argv[1],'rb')
+name = os.path.basename(sys.argv[1]).split('.')[0]
+output_dir = os.path.dirname(sys.argv[1])
+fw_data = bytearray(fw_bin.read())
+app_version = 0x1
+reserved = 0x0
+app_magic = b'LSEC_APP'
+sign_and_image_data = fw_data[APP_SIGN_OFFSET:]
+sign_and_image_size = len(sign_and_image_data)
+sign_and_image_crc32 = zlib.crc32(sign_and_image_data)
+app_desc_head = struct.pack('8sIIIII', app_magic, sign_and_image_size, app_version, reserved, reserved, sign_and_image_crc32)
+app_desc_crc_val = zlib.crc32(app_desc_head)
+app_desc_crc = struct.pack('I', app_desc_crc_val)
+app_image_header = app_desc_head + app_desc_crc
+app_image_header_len = len(app_image_header)
+# 添加app的header
+fw_data[APP_DESC_OFFSET : APP_DESC_OFFSET + app_image_header_len] = app_image_header
+f_total = open(os.path.join(output_dir,name+'.bin'),'wb')
+f_total.write(fw_data)
+f_total.close()
+
+# 生成HEX文件
+def bin_to_hex_with_intelhex(bin_file_path, hex_file_path, start_address=0x08000000):
+    try:
+        ih = IntelHex() 
+        ih.loadbin(bin_file_path, offset=start_address)
+        ih.write_hex_file(hex_file_path)
+        print(f"Successfully generated HEX file: {hex_file_path}")
+    except FileNotFoundError:
+        print(f"Error: BIN file not found at {bin_file_path}", file=sys.stderr)
+    except Exception as e:
+        print(f"An unexpected error occurred during HEX conversion: {e}", file=sys.stderr)
+hex_file_path = os.path.join(output_dir, name + '.hex')
+final_bin_file_path = os.path.join(output_dir, name + '.bin')
+bin_to_hex_with_intelhex(final_bin_file_path, hex_file_path, start_address=0x08000000)
+
+#生成BIOS文件
 split_ptr = not (len(sys.argv) == 2)
 if split_ptr:
     desc_offset = int(sys.argv[2],16)
@@ -20,17 +63,12 @@ int_flash_search_loop = 0x0
 int_flash_search_delay_10us = 0x0
 desc_ptr_head = struct.pack('4sIHH',magic,desc_offset,int_flash_search_loop,int_flash_search_delay_10us)
 desc_ptr_crc = struct.pack('I',zlib.crc32(desc_ptr_head))
-
-fw_bin = open(sys.argv[1],'rb')
-name = os.path.basename(sys.argv[1]).split('.')[0]
-output_dir = os.path.dirname(sys.argv[1])
-fw_data = fw_bin.read()
 image_version = int.from_bytes(fw_data[0x1c:0x20],'little')
-fw_len = len(fw_data)
-fw_crc32 = zlib.crc32(fw_data)
+info_sbl_data = fw_data[:INFO_SBL_DATA_LEN]
+info_sbl_crc32 = zlib.crc32(info_sbl_data)
 image_offset = 0x20
 copy_strategy = FLASH_CHIP_ERASE<<1|COPY_TO_RAM
-desc_head = struct.pack('IIIIIII',image_offset,fw_len,image_version,fw_crc32,copy_strategy,RAM_DST_ADDR,RAM_ENTRY_ADDR)
+desc_head = struct.pack('IIIIIII',image_offset,INFO_SBL_DATA_LEN,image_version,info_sbl_crc32,copy_strategy,RAM_DST_ADDR,RAM_ENTRY_ADDR)
 desc_crc = struct.pack('I',zlib.crc32(desc_head))
 
 if split_ptr:
