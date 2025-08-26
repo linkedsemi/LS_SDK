23abbad320e6a3c4c8d455ac6358b7f98ac1bb80
Xintian Zhang <xtzhang@linkedsemi.com>
2025-08-26 19:04:22 +0800

    FLASH_API_ALL_IN_RAM

--- hal_driver/src/ls_hal_flash_swint.c
+++ hal_driver/src/ls_hal_flash_swint.c
@@ -23,53 +23,53 @@
         }\
     }while(0)
 
-void hal_flashx_program_operation(struct hal_flash_env *env,void *param)
+void FLASH_API_SECTION(hal_flashx_program_operation,struct hal_flash_env *env,void *param)
 {
     FLASH_EXECUTE_NO_RETVAL(do_hal_flashx_program,env,param);
 }
 
-void hal_flashx_write_reg_operation(struct hal_flash_env *env,void *param)
+void FLASH_API_SECTION(hal_flashx_write_reg_operation,struct hal_flash_env *env,void *param)
 {
     FLASH_EXECUTE_NO_RETVAL(do_hal_flashx_write_reg,env,param);
 }
 
-void hal_flashx_read_operation(struct hal_flash_env *env,void*param)
+void FLASH_API_SECTION(hal_flashx_read_operation,struct hal_flash_env *env,void*param)
 {
     FLASH_EXECUTE_NO_RETVAL(do_hal_flashx_read,env,param);
 }
 
-void hal_flashx_read_reg_operation(struct hal_flash_env *env,void *param)
+void FLASH_API_SECTION(hal_flashx_read_reg_operation,struct hal_flash_env *env,void *param)
 {
     FLASH_EXECUTE_NO_RETVAL(do_hal_flashx_read_reg,env,param);
 }
 
-void hal_flashx_chip_erase_operation(struct hal_flash_env *env,void *param)
+void FLASH_API_SECTION(hal_flashx_chip_erase_operation,struct hal_flash_env *env,void *param)
 {
     FLASH_EXECUTE_NO_RETVAL(do_hal_flashx_chip_erase,env,param);
 }
 
 #ifndef __CC_ARM
-NOINLINE void do_hal_flashx_program_swint(struct hal_flash_env *env,void *param)
+NOINLINE void FLASH_API_SECTION(do_hal_flashx_program_swint,struct hal_flash_env *env,void *param)
 {
     SWINT_FUNC_CALL_INLINE_ASM(do_hal_flashx_program,FLASH_SWINT_NUM);
 }
 
-NOINLINE void do_hal_flashx_write_reg_swint(struct hal_flash_env *env,void *param)
+NOINLINE void FLASH_API_SECTION(do_hal_flashx_write_reg_swint,struct hal_flash_env *env,void *param)
 {
     SWINT_FUNC_CALL_INLINE_ASM(do_hal_flashx_write_reg,FLASH_SWINT_NUM);
 }
 
-NOINLINE void do_hal_flashx_read_swint(struct hal_flash_env *env,void *param)
+NOINLINE void FLASH_API_SECTION(do_hal_flashx_read_swint,struct hal_flash_env *env,void *param)
 {
     SWINT_FUNC_CALL_INLINE_ASM(do_hal_flashx_read,FLASH_SWINT_NUM);
 }
 
-NOINLINE void do_hal_flashx_read_reg_swint(struct hal_flash_env *env,void *param)
+NOINLINE void FLASH_API_SECTION(do_hal_flashx_read_reg_swint,struct hal_flash_env *env,void *param)
 {
     SWINT_FUNC_CALL_INLINE_ASM(do_hal_flashx_read_reg,FLASH_SWINT_NUM);
 }
 
-NOINLINE void do_hal_flashx_chip_erase_swint(struct hal_flash_env *env,void *param)
+NOINLINE void FLASH_API_SECTION(do_hal_flashx_chip_erase_swint,struct hal_flash_env *env,void *param)
 {
     SWINT_FUNC_CALL_INLINE_ASM(do_hal_flashx_chip_erase,FLASH_SWINT_NUM);
 }
