b7bde0e4b9e97d21ba186cc27f4f589b5696f89d
zxtxin <xtzhang@linkedsemi.com>
2025-09-17 12:11:47 +0800

    sw_fifo_element_amount bugfix

--- module/static_buffer/fifo.c
+++ module/static_buffer/fifo.c
@@ -31,7 +31,13 @@ ROM_SYMBOL void sw_fifo_flush(struct fifo_env *ptr)
 
 ROM_SYMBOL uint16_t sw_fifo_element_amount(struct fifo_env *ptr)
 {
-    return abs(ptr->wr_idx-ptr->rd_idx);
+    if(ptr->wr_idx>=ptr->rd_idx)
+    {
+        return ptr->wr_idx-ptr->rd_idx;
+    }else
+    {
+        return 2*ptr->length+ptr->wr_idx-ptr->rd_idx;
+    }
 }
 
 ROM_SYMBOL LL_PKT_ISR bool general_fifo_put(struct fifo_env *ptr,void *data)
