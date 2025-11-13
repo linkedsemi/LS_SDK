0d48600b38c751cff21b2ebd5fd34f03e2688a69
Hunter Liu <htliu@linkedsemi.com>
2025-11-13 22:50:55 +0800

    qsh: update bootram

--- bootloader/boot_ram/qsh/app_config.h
+++ bootloader/boot_ram/qsh/app_config.h
@@ -1,6 +1,9 @@
 #ifndef APP_CONFIG_H_
 #define APP_CONFIG_H_
 
-
+//LOG_UART
+#define LOG_BACKEND (2)
+#undef LOG_TAG
+#define LOG_TAG     "bootram"
 
 #endif
--- bootloader/boot_ram/qsh/boot_ram.c
+++ bootloader/boot_ram/qsh/boot_ram.c
@@ -1,4 +1,5 @@
 #include "platform.h"
+#include "otp_config.h"
 #include "boot_port.h"
 #include "boot.h"
 #include "log.h"
@@ -6,19 +7,27 @@
 #include "reg_sysc_sec_cpu.h"
 #include "reg_sysc_sec_awo.h"
 #include "reg_sec_pmu_rg.h"
+#include "ls_hal_otp_ctrl.h"
 #include "ls_hal_flash.h"
 #include "ls_hal_crc.h"
 #include "ls_hal_qspiv2.h"
 #include "ls_hal_cache.h"
+#include "cpu.h"
 #include "ls_dbg.h"
 
 extern uint32_t __next_ram_size;
+struct boot_otp otp_cfg;
 
 __attribute__((aligned(4))) uint8_t temp_buf[TEMP_BUF_SIZE];
 
 void boot_flash_read(uint32_t offset, uint8_t *data, uint32_t length)
 {
-    hal_flash_fast_read(offset, data, length);
+    hal_flash_multi_io_read(offset, data, length);
+}
+
+void boot_otp_read(uint32_t offset, uint8_t *data, uint32_t length)
+{
+    HAL_OTP_Read(offset, data, length);
 }
 
 uint32_t boot_crc32(uint8_t *data, uint32_t length)
@@ -68,41 +77,17 @@ void goto_next(void *exe_addr)
     goto_sbl();
 }
 
-void rom_delay(uint32_t us)
-{
-    uint32_t CLK_MHZ = 0;
-    switch (REG_FIELD_RD(SYSC_SEC_AWO->PD_AWO_CLK_CTRL0, SYSC_SEC_AWO_CLK_SEL_HBUS))
-    {
-    case SEC_AWO_REG_CLK_SEL_HSI:
-    case SEC_AWO_REG_CLK_SEL_HSE:
-    case SEC_AWO_REG_CLK_SEL_CLIKIN:
-        CLK_MHZ = 25;
-        break;
-    case SEC_AWO_REG_CLK_SEL_DPLL_DIV:
-        CLK_MHZ = 600 / (REG_FIELD_RD(SYSC_SEC_AWO->PD_AWO_CLK_CTRL0, SYSC_SEC_AWO_CLK_DIV_PARA_HBUS_M1) + 1);
-        break;
-    case SEC_AWO_REG_CLK_SEL_DPLL_600M:
-        CLK_MHZ = 600;
-        break;
-    default:
-        LS_ASSERT(0);
-        break;
-    }
-#ifdef FPGA_TEST
-    CLK_MHZ = SDK_HCLK_MHZ;
-#endif
-    rv32_delay_asm((us) * (CLK_MHZ) / 8, 1);
-}
-
 static void set_runtime_cfg()
 {
-    if ((0 == READ_BIT(SYSC_SEC_AWO->DPLL_LOCK, SYSC_SEC_AWO_DPLL1_LOCK_MASK))
-        && (0 == READ_BIT(SYSC_SEC_AWO->DPLL_LOCK, SYSC_SEC_AWO_DPLL2_LOCK_MASK))) {
-    } else {
-        app_cpu_reset();
+    log_en = otp_cfg.startup_param.log_uart_en;
+    if (log_en) {
+        log_output_fn = log_output;
+        log_hex_output_fn = log_hex_output;
     }
 
-    pinmux_hal_flash_quad_init();
+    qspiv2_global_int_disable_fn = enter_critical;
+    qspiv2_global_int_restore_fn = exit_critical;
+
     flash1.reg = (void *)LSQSPIV2;
     flash1.dual_mode_only = false;
     flash1.continuous_mode_enable = false;
@@ -110,16 +95,8 @@ static void set_runtime_cfg()
     flash1.suspend_count = 0;
     flash1.continuous_mode_on = false;
     flash1.addr4b = false;
-    hal_flash_init();
-    clk_flash_init();
 
-    LOG_I("flash reset...");
-    hal_flash_release_from_deep_power_down();
-    rom_delay(20);
-    hal_flash_software_reset();
-    rom_delay(200);
-
-    hal_flash_qe_status_read_and_set();
+    pinmux_hal_flash_quad_init();
 }
 
 static void boot_flash_swint_init()
@@ -136,13 +113,10 @@ void platform_init()
 {
     __enable_irq();
 
+    boot_otp_read(0x0, (uint8_t *)&otp_cfg, sizeof(otp_cfg));
     set_runtime_cfg();
-    LOG_I("OTP CFG_DATA:");
-    for (uint32_t i = 0; i < OTP_LOG_WORD_LENGTH; i += 4)
-        LOG_I("0x%02x:\t0x%08x\t0x%08x\t0x%08x\t0x%08x", i * 4, ptr[i], ptr[i + 1], ptr[i + 2], ptr[i + 3]);
 
     boot_flash_swint_init();
-
     uint8_t jedec_id[3] = {};
     hal_flashx_read_id(&flash1, jedec_id);
     if ((jedec_id[0] != 0) && (jedec_id[1] != 0) && (jedec_id[2] != 0)) {
@@ -152,7 +126,7 @@ void platform_init()
     } else {
         while(1);
     }
-
+    hal_flash_qe_status_read_and_set();
     HAL_LSCRC_Init();
 }
 
