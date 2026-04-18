de6bddb597440f0c237041e881115355ed58c0d1
houruiying <ryhou@linkedsemi.com>
2026-04-18 19:47:25 +0800

    Delete unnecessary definitions

--- soc/rv32/raptor/platform.c
+++ soc/rv32/raptor/platform.c
@@ -21,7 +21,6 @@
 // #define PMU_CLK_VAL (SDK_HSE_USED << V33_RG_CLK_SET_HSE_POS | 1 << V33_RG_CLK_SET_HSI_POS | (!SDK_LSI_USED) << V33_RG_CLK_SET_LSE_POS)
 
 __attribute__((aligned(64))) void (*interrupt_vector[IRQN_MAX])();
-struct SSIV2_HandleTypeDef flash;
 
 __attribute__((weak)) void SystemInit(){
     e906_init();
@@ -126,7 +125,6 @@ __attribute__((weak)) void SystemInit(){
 
 void sys_init_none()
 {
-    flash.reg = LSSSIV2;
 
 //     clk_flash_init();
 //     // set_all_irq_priority_to_lowest_level();
