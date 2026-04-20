6b9a7f424b717b558d7c424ccb007369cdc39d81
houruiying <ryhou@linkedsemi.com>
2026-04-20 19:59:37 +0800

    update

--- soc/rv32/raptor/ls_msp_iwdg.c
+++ soc/rv32/raptor/ls_msp_iwdg.c
@@ -6,7 +6,7 @@ void HAL_IWDG_MSP_Init(reg_iwdg_t *iwdg)
     switch ((uint32_t)iwdg)
     {
     case (uint32_t)IWDT0:
-        CPU0_SYS_CFG -> WDT0_LRC_CLK_CLK_DEGLITCH_SW2_REG = 0x2; //0x1:32Khz 0x2:50Mhz //TODO
+        CPU0_SYS_CFG -> WDT0_LRC_CLK_CLK_DEGLITCH_SW2_REG = 0x1; //0x1:32Khz 0x2:50Mhz //TODO
         CPU0_SYS_CFG -> WDT0_LRC_CLK_CLK_CG0_REG = 0x1;
         REG_FIELD_WR(CPU0_SYS_CFG -> CPU0_SYS_CFG_REG2, CPU0_SYS_WDT0_APB_CLK_CLK_CG0_2, 0x1);
         break;
