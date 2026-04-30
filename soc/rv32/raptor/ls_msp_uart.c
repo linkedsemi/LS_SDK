42b95febd0832bb5a927684b4265b0150fc5e27f
houruiying <ryhou@linkedsemi.com>
2026-04-30 15:46:24 +0800

    fixbug: dwuart

--- soc/rv32/raptor/ls_msp_uart.c
+++ soc/rv32/raptor/ls_msp_uart.c
@@ -347,11 +347,9 @@ void LL_DWUART2_MSP_DeInit(void)
 
 
 //uart_log
-__attribute__((weak)) void LL_DWUART8_Handler() {}
-
 void LL_DWUART8_MSP_Init(void)
 {
-    rv_set_int_isr(CPU_DWUART8_IRQn, LL_DWUART8_Handler);
+    rv_set_int_isr(CPU_DWUART8_IRQn, DWUART8_Handler);
     // REG_FIELD_WR(CPU0_SYS_CFG->CPU0_SYS_CFG_REG4, CPU0_SYS_UART8_APB_CLK_CLK_CG0_1, 0x0);
     // CPU1_SYS_CFG->TOP_DIG_CRG_TOP_SYS0_APB_CLK_TOP_RST_N_REG0_CLR = 0x8;
     // CPU1_SYS_CFG->TOP_DIG_CRG_TOP_SYS0_APB_CLK_TOP_RST_N_REG0 = 0x8;
@@ -363,11 +361,9 @@ void LL_DWUART8_MSP_DeInit(void)
     REG_FIELD_WR(CPU0_SYS_CFG->CPU0_SYS_CFG_REG4, CPU0_SYS_UART8_APB_CLK_CLK_CG0_1, 0x0);
 }
 
-__attribute__((weak)) void LL_DWUART12_Handler() {}
-
 void LL_DWUART12_MSP_Init(void)
 {
-    rv_set_int_isr(PERIPH_DWUART12_IRQn, LL_DWUART12_Handler);
+    rv_set_int_isr(PERIPH_DWUART12_IRQn, DWUART12_Handler);
     REG_FIELD_WR(CPU1_SYS_CFG->PERIPH_SYS_CFG_REG2, PERIPH_SYS_UART12_APB_CLK_CLK_CG0_3, 0x1);
 }
 
