22b15c25f6bfd5cacc32fc68662b7b646e9c75bb
XintianZhang <xtzhang@linkedsemi.com>
2026-07-11 18:06:17 +0800

    fix build warning

--- soc/rv32/qsh/ls_soc_gpio.c
+++ soc/rv32/qsh/ls_soc_gpio.c
@@ -823,7 +823,7 @@ static uint8_t pin2func_io(gpio_port_pin_t *x)
 void XIP_BANNED_FUNC(per_func_enable,uint8_t pin, uint8_t func_num)
 {
     gpio_port_pin_t *x = (gpio_port_pin_t *)&pin;
-    SYSC_APP_AWO->IO_FUNC[func_num][x->port >> 1] |= 1 << (((x->port % 2) * 16)+ x->num);
+    SYSC_APP_AWO->IO_FUNC[func_num&0x3][x->port >> 1] |= 1 << (((x->port % 2) * 16)+ x->num);
 }
 
 bool is_per_func_enable(uint8_t pin, uint8_t func_num)
